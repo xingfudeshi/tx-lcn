0d87cf333f6a16ea7c5361b39a800786fa415ccb
wu <xingfudeshi@vip.qq.com>
2019-04-28 14:17:54 +0800

    compatible with alibaba dubbo

---
+++ txlcn-tracing/src/main/java/com/codingapi/txlcn/tracing/alidubbo/TxlcnRoundRobinLoadBalance.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017-2019 CodingApi .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codingapi.txlcn.tracing.alidubbo;
+
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.cluster.loadbalance.RoundRobinLoadBalance;
+
+import java.util.List;
+
+/**
+ * Description:
+ * Company: CodingApi
+ * Date: 2018/12/14
+ *
+ * @author ujued
+ */
+public class TxlcnRoundRobinLoadBalance extends RoundRobinLoadBalance {
+
+    @Override
+    public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) {
+        return DubboTxlcnLoadBalance.chooseInvoker(invokers, url, invocation, super::select);
+    }
+}
